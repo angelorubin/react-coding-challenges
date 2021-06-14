67ba5ce119ae98ad73358994ff4d2ab5ffc4af76
angelorubin <angelorubin@gmail.com>
2021-06-14 12:17:39 -0300

    Criando componente NoMatch

---
+++ dark-mode/src/routes/NoMatch/index.js
@@ -0,0 +1,11 @@
+import "./_no-match.scss";
+
+const NoMatch = () => {
+  return (
+    <div className="no-match">
+      <h1 className="no-match__title">Page Not Found</h1>
+    </div>
+  );
+};
+
+export default NoMatch;
