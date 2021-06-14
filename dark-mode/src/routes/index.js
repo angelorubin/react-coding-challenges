97f26a523d15a6b8f83620749651beac028774d6
angelorubin <angelorubin@gmail.com>
2021-06-14 12:15:42 -0300

    Criando rotas

--- dark-mode/src/routes/index.js
+++ dark-mode/src/routes/index.js
@@ -1,5 +1,24 @@
-import Home from './App';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import NoMatch from "./NoMatch";
 
 // Use something like react-router-dom to manage multiple pages/routes
-
-export default Home;
+export const Routes = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route>
+          <NoMatch />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
