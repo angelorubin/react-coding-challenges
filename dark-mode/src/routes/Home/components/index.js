7b49ded21459abb2570605638866ea1e46609e28
angelorubin <angelorubin@gmail.com>
2021-06-14 12:27:20 -0300

    Arquivo renomeado

---
+++ dark-mode/src/routes/Home/components/index.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import "../styles/_home.scss";
+import { useDarkMode } from "hooks/useDarkMode";
+
+function Home() {
+  const [darkMode, setDarkMode] = useDarkMode();
+
+  return (
+    <div className={`home ${darkMode}`}>
+      <div className="level">
+        <div>
+          <h1 className="title">Dark Mode Challenge</h1>
+        </div>
+
+        {/* --The button that should toggle dark mode-- */}
+        <button
+          className="app__dark-mode-btn icon level-right"
+          onClick={setDarkMode}
+        >
+          <FontAwesomeIcon
+            icon={darkMode === "dark-mode" ? faSun : faMoon}
+            color={darkMode === "dark-mode" ? "#FFA500" : ""}
+          />
+        </button>
+      </div>
+
+      <div className="columns">
+        <div className="column">
+          <p>
+            Lollipop powder powder. Cotton candy caramels chupa chups halvah
+            muffin caramels apple pie topping cake. Topping chocolate bar pastry
+            chocolate cake. Cupcake tart jujubes dragée jelly-o icing sugar
+            plum. Chocolate bar lollipop candy canes. Biscuit croissant apple
+            pie pudding caramels wafer tart tootsie roll macaroon. Croissant
+            tiramisu chocolate bar carrot cake lemon drops halvah.
+          </p>
+        </div>
+        <div className="column">
+          <p>
+            Marshmallow tiramisu liquorice bear claw chocolate bar bear claw
+            tart. Muffin chupa chups pie. Brownie apple pie topping lemon drops
+            marzipan toffee. Pudding macaroon icing ice cream bonbon cake tart.
+            Pudding sugar plum chocolate cake cake biscuit pastry pastry
+            chocolate bar tart. Lemon drops dessert gummies icing.
+          </p>
+        </div>
+      </div>
+
+      <div className="field">
+        <div className="control">
+          <input className="input" type="text" placeholder="Name" />
+        </div>
+      </div>
+
+      <div className="field">
+        <div className="control">
+          <input className="input" type="text" placeholder="Email" />
+        </div>
+      </div>
+
+      <section className="section">
+        <div className="buttons level-right">
+          <button className="button is-primary">Save</button>
+          <button className="button is-link">Submit</button>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Home;
