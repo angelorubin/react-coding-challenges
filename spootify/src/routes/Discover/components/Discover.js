5c09cd34b0fe575f088476076a621b6ba03f7a98
Angelo Rubin <angelorubin@gmail.com>
2021-06-17 13:59:30 -0300

    Removendo chaves de acesso do spotify app

--- spootify/src/routes/Discover/components/Discover.js
+++ spootify/src/routes/Discover/components/Discover.js
@@ -12,17 +12,6 @@ const Discover = () => {
   });
 
   useEffect(() => {
-    const client_id = "db6d9d593dfa403db0c080c0eff7f7b3";
-    const client_secret = "94f5e73dc2dc49738e2d896489d6c9c6";
-    const redirect_uri = encodeURIComponent("https://www.terra.com.br");
-    let scopes = encodeURIComponent("user-read-private user-read-email");
-    const response_type = "code";
-    const url = "https://accounts.spotify.com/api/token";
-    const headers = {
-      "Access-Control-Allow-Credentials": false,
-      Accept: "application/json",
-    };
-
     http({
       method: "post",
       url: "/api/token",
